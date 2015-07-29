5927c9e570cd01e9c876be4142713808623d5afb
Huy Quang Ngo <manhle@Huys-Mac-mini.local>
2015-07-29 10:46:33 +0700

    Student

---
+++ Selector/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  Selector
+//
+//  Created by Huy Quang Ngo on 7/29/15.
+//  Copyright (c) 2015 Khanh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ Selector/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  Selector
+//
+//  Created by Huy Quang Ngo on 7/29/15.
+//  Copyright (c) 2015 Khanh. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
